52831742f65247c09d9675016aa0c00745553db8
Marcio Trindade <marciotrindade@gmail.com>
2015-03-30 01:14:35 -0300

    melhorando os testes da calculadora

--- aula3/spec/calculadora_spec.rb
+++ aula3/spec/calculadora_spec.rb
@@ -1,21 +1,40 @@
 require_relative '../calculadora'
 
-RSpec.describe Calculadora do 
-  describe "teste calculator" do
-    it "calcula soma" do
-      expect(subject.soma 1,2).to eql 3 
-    end 
-    it "calcula divide" do
-      expect(subject.divide 10,2).to eql 5
-    end 
-    it "calcula multiplica" do
-      expect(subject.multiplica 3,5).to eql 15
-    end 
-    it "calcula subtrai" do
-      expect(subject.subtrai 10,8).to eql 2
-    end 
-    it "raises" do
-      expect{subject.divide 3,0}.to raise_error(ZeroDivisionError)
+RSpec.describe Calculadora do
+  describe '#soma' do
+    let(:resultado) { subject.soma(1,2) }
+
+    it 'deve retornar a soma dos 2 atributos' do
+      expect(resultado).to eql(3)
     end
-  end  
+  end
+
+  describe '#subtrai' do
+    let(:resultado) { subject.subtrai(10,8) }
+    it 'deve retornar a subtração dos 2 atributos' do
+      expect(resultado).to eql(2)
+    end
+  end
+
+  describe '#mutiplica' do
+    let(:resultado) { subject.multiplica(3,5) }
+    it 'deve retornar a mutiplicação dos 2 atributos' do
+      expect(resultado).to eql(15)
+    end
+  end
+
+  describe '#divide' do
+    let(:resultado) { subject.divide(10,2) }
+    it 'deve retornar o resultado' do
+      expect(resultado).to eql(5)
+    end
+
+    context 'quando divide por zero' do
+      let(:resultado) { subject.divide(3,0) }
+
+      it 'deve lança o erro de divisaão por zero' do
+        expect{ resultado }.to raise_error(ZeroDivisionError)
+      end
+    end
+  end
 end
