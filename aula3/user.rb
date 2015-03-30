5eac0e222b7995e95926cac266047f836b7f76d7
Marcio Trindade <marciotrindade@gmail.com>
2015-03-30 01:14:20 -0300

    refatorando o user

--- aula3/user.rb
+++ aula3/user.rb
@@ -1,17 +1,3 @@
 class User
-  def name
-    @name
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def age
-    @age
-  end
-
-  def age=(age)
-    @age = age
-  end
+  attr_accessor :name, :age
 end
